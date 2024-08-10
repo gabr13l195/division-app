b51e675ec3b04f8d9d208f6afae7b54a035b760c
Wilmer Betancourt <wilmergabybet@gmail.com>
2024-08-10 12:15:12 -0500

    Creando repositorio

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Pantalla1Screen } from './src/screens/Pantalla1Screen';
+
+const App = () => {
+  return (
+    <View>
+      <Pantalla1Screen/>
+    </View>
+  )
+}
+
+export default App;
---
+++ src/screens/Pantalla1Screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const Pantalla1Screen = () => {
+    return (
+        <View>
+            <Text>
+                Hola
+            </Text>
+        </View>
+    )
+}
